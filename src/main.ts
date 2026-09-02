7afde2e0523d9cb39f7f9f0dc52f73b69d82607b
Giridhar <balinenigiri8@gmail.com>
2026-09-02 14:13:34 +0530

    Databinding of class to its templates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Component } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: 
+      `
+        <h1>Hello, {{personName}}</h1>
+        <button (click)="onButtonClick()">Click Here</button>
+      `
+})
+class App {
+  personName: string = "Nina";
+
+  onButtonClick() {
+    this.personName = "Giridhar";
+  }
+}
+
+bootstrapApplication(App);
